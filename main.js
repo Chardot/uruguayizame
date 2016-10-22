88d33244df9fb495eb7b25be197ef8937d9d2e7e
Rossina Simonelli <rossina.simonelli@gmail.com>
2016-10-21 22:55:15 -0300

    First funtionality added

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
+var map = [];
+
 jQuery( document ).ready(function( $ ) {
 	$.ajax({
-		url: "https://spreadsheets.google.com/feeds/list/1qvf8NYiMuS-kMRrdu-mXve_dWNWH80HCVoSugEXunXI/1/public/values?alt=json",
-		success: function( result ) {
-			debugger;
-			$( "#main1" ).text(result.feed.entry[0]['gsx$title1']['$t']);
-			$( "#main2" ).text(result.feed.entry[0]['gsx$title2']['$t']);
-			$( "#main3" ).text(result.feed.entry[0]['gsx$title3']['$t']);
+		url: "https://spreadsheets.google.com/feeds/list/1hyX9RIS9cza_WXLYCParWALZIfSnmpt0qSb5xszGfog/1/public/values?alt=json",
+        success: function( result ) {
+            var a = result.feed.entry;
+            for (index = 0; index < a.length; ++index) {
+                var e = new Object();
+                e.input = result.feed.entry[index]['gsx$input']['$t'];
+                e.output = result.feed.entry[index]['gsx$output']['$t']
+                map.push(e);    
+            }
 		}
 	});
 }); 
+
+$(document).ready(function() {
+    function escapeRegExp (s) {
+        return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    };
+
+    $("#submit").click(function() {
+        $.grep(map, function(e){
+            var userinput = $("#inputText").val();
+            if (userinput.toLowerCase().indexOf(' '+ e.input + ' ') !== -1 ||
+                userinput.toLowerCase().indexOf(' '+ e.input) !== -1 ||
+                userinput.toLowerCase().indexOf(e.input + ' ') !== -1 ){
+                $("#result").text(userinput.replace(e.input, e.output));
+            } 
+        });
+        
+    });
+
+});
